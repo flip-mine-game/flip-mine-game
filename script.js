e2fb3e03d0d20b2a012959aefc01064b37fa175a
flip-mine-game <meganft164@gmail.com>
2026-02-01 15:30:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let saldo = 0;
-
 function entrar() {
   window.location.href = "dashboard.html";
-}
-
-function claim() {
-  saldo += 0.01;
-  alert("Você ganhou FLIP fake!");
-}
-
-function ptc() {
-  saldo += 0.02;
-  alert("Anúncio fake assistido!");
 }
