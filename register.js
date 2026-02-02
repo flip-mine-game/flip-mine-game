78a71923bbc0c683c127f7bdfc83f28672c06ac9
flip-mine-game <meganft164@gmail.com>
2026-02-02 17:39:50 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,6 @@
-
+import fs from'fs';
+export default (req,res)=>{
+ const p='data/users.json'; const u=JSON.parse(fs.readFileSync(p));
+ const {email,password}=req.body; u[email]={password,balance:0,today:0,total:0};
+ fs.writeFileSync(p,JSON.stringify(u,null,2)); res.json({success:true});
+}
