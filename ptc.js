b2d80d4af69bfa88c20280c1fed162ed1cbe0698
flip-mine-game <meganft164@gmail.com>
2026-02-02 17:41:37 -0300

    Update ptc.js

--- ptc.js
+++ ptc.js
@@ -1,1 +1,6 @@
-
+import fs from'fs';
+export default (req,res)=>{
+ const p='data/users.json'; const u=JSON.parse(fs.readFileSync(p));
+ const e=req.body.email; u[e].balance+=0.005; u[e].today+=0.005; u[e].total+=0.005;
+ fs.writeFileSync(p,JSON.stringify(u,null,2)); res.json({ok:true});
+}
