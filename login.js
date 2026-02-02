06ed23e6e8b1f5fc6015c2053fdf4dcbbd0a7761
flip-mine-game <meganft164@gmail.com>
2026-02-02 17:40:36 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,6 @@
-
+import fs from'fs';
+export default (req,res)=>{
+ const u=JSON.parse(fs.readFileSync('data/users.json'));
+ const {email,password}=req.body;
+ res.json({success: u[email]&&u[email].password===password});
+}
