e1c5dce5aa4e1ef75d359b471916288b6b3afa26
flip-mine-game <meganft164@gmail.com>
2026-02-02 20:45:56 -0300

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,1 +1,18 @@
-
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: '#111',
+      color: '#fff',
+      textAlign: 'center',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1 style={{ fontSize: '36px', fontWeight: 'bold', color: 'orange' }}>
+        🚧 Atenção: Estamos testando o site! 🚧
+      </h1>
+    </div>
+  )
+  }
